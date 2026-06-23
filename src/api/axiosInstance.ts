c10f2f3d0cfa4e9096844c6d67e2cd7dafbd54b4
julio <jbermudezfc90@gmail.com>
2026-06-23 19:01:36 -0400

    add loader for render server wake up

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,4 +1,14 @@
 import axios from 'axios';
+import { toast } from 'sonner';
+
+declare module 'axios' {
+    interface InternalAxiosRequestConfig {
+        _wakeupTimer?: ReturnType<typeof setTimeout>;
+    }
+}
+
+const WAKEUP_TOAST_ID = 'server-wakeup';
+const WAKEUP_THRESHOLD_MS = 3000;
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
@@ -13,7 +23,29 @@ api.interceptors.request.use((config) => {
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
+
+    config._wakeupTimer = setTimeout(() => {
+        toast.loading('Server is waking up…', {
+            id: WAKEUP_TOAST_ID,
+            description: 'This may take a few seconds.',
+            duration: Infinity,
+        });
+    }, WAKEUP_THRESHOLD_MS);
+
     return config;
 });
+
+api.interceptors.response.use(
+    (response) => {
+        clearTimeout(response.config._wakeupTimer);
+        toast.dismiss(WAKEUP_TOAST_ID);
+        return response;
+    },
+    (error) => {
+        clearTimeout(error.config?._wakeupTimer);
+        toast.dismiss(WAKEUP_TOAST_ID);
+        return Promise.reject(error);
+    },
+);
 
 export default api;
